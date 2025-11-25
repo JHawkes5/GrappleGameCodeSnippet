bfa3f13238a5add961622adfd6ca56a10845d0d8
JHawkes5 <23651725@stu.mmu.ac.uk>
2025-11-25 21:05:20 +0000

    Cleaned null space + comment

--- Hookshot.cs
+++ Hookshot.cs
@@ -24,7 +24,6 @@ public class Hookshot : MonoBehaviour
     private Vector3 grapplePoint;
     private DistanceJoint2D joint;
 
-
     // Start is called before the first frame update
     void Start()
     {
@@ -33,8 +32,6 @@ public class Hookshot : MonoBehaviour
      rope.enabled = false;
      aimRope.enabled = true;
     }
-
-
 
     // Update is called once per frame
     void Update()
@@ -45,8 +42,7 @@ public class Hookshot : MonoBehaviour
         
         Vector3 currentPosition = transform.position;
         Vector3 direction = (new Vector3(currentPosition.x + x, currentPosition.y + y, 0) - currentPosition).normalized;
-        //Debug.DrawLine(currentPosition, currentPosition + direction * 10, Color.red, 0.0f, false);
-
+        
         RaycastHit2D hit = Physics2D.Raycast(//casts a ray, returning a value if it hits a valid target on the grapple layer
         origin: currentPosition,
         direction: direction,
@@ -56,8 +52,6 @@ public class Hookshot : MonoBehaviour
         //draws reticle for aiming
         aimRope.SetPosition(0, currentPosition); 
         aimRope.SetPosition(1, currentPosition + direction);
-
-
 
         //Changes colour of aiming reticle if pointing at  valid target
         if (hit.collider != null)
@@ -69,19 +63,12 @@ public class Hookshot : MonoBehaviour
         {
             aimRope.startColor = fadedRed;
             aimRope.endColor = fadedRed;
-        }
-
-     
-
-
-
+        }  
 
         if (Input.GetButtonDown("Fire1"))
-        { //activates on left shift and left mouse button                      
-            
+        { //activates on left shift and left mouse button down                   
             if (hit.collider != null) //must be valid target
             {
-
                 if (hit.collider.name == "Platform") //if ray hits platform object link to platform's rigidbody
                 {
                     joint.connectedAnchor = Vector2.zero; 
@@ -90,10 +77,7 @@ public class Hookshot : MonoBehaviour
                     joint.distance = grappleLength;
                     rope.enabled = true;
 
-                    ConnectedPlatformLocation = hit.collider.gameObject.GetComponent<Transform>();
-                    
-
-                    
+                    ConnectedPlatformLocation = hit.collider.gameObject.GetComponent<Transform>();                                        
                 }
                 else //if connected to wall link player to coordinates where ray hit
                 {
@@ -104,30 +88,15 @@ public class Hookshot : MonoBehaviour
                     joint.enabled = true;
                     joint.distance = grappleLength;
                     rope.enabled = true;
-                }
-
-                //Debug.Log(hit.collider.name);
-
-
-                
-
-                
-
-
+                }      
             }
-
-
         }
 
-        if(Input.GetButtonUp("Fire1"))
-        {
-            
+        if(Input.GetButtonUp("Fire1")) //activates on left shift and left mouse button release
+        {            
             joint.enabled = false;
             rope.enabled = false;
             joint.connectedBody = null;
-
-
-
         } //releases grapple
 
         if (rope.enabled == true) //updates rope renderer
@@ -136,26 +105,12 @@ public class Hookshot : MonoBehaviour
             {
                 //joint.distance = grappleLength;
                 rope.SetPosition(0, new Vector3(ConnectedPlatformLocation.position.x, ConnectedPlatformLocation.position.y, 0));
-
             }
             else
             {
                 rope.SetPosition(0, grapplePoint);
-            }
-                
-
-
-            
+            }                       
             rope.SetPosition(1, transform.position);
-            
-
         }
-
-
     }
-
-
-
-
-
 }
